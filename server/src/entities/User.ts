b4665ce79a4873b7ef615c3485d39e431695449b
Jeff Zhang <zhang.jeffreyd@gmail.com>
2020-05-02 13:15:03 -0400

    Server: Create user entity

---
+++ server/src/entities/User.ts
@@ -0,0 +1,46 @@
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm'
+import { ObjectType, Field, ID, registerEnumType } from 'type-graphql'
+
+enum Permission {
+  ADMIN = 'ADMIN',
+  USER = 'USER',
+  ITEMCREATE = 'ITEMCREATE',
+  ITEMUPDATE = 'ITEMUPDATE',
+  ITEMDELETE = 'ITEMDELETE',
+  PERMISSIONUPDATE = 'PERMISSIONUPDATE',
+}
+
+registerEnumType(Permission, {
+  name: 'Permission',
+})
+
+@ObjectType()
+@Entity()
+class User extends BaseEntity {
+  @Field((type) => ID)
+  @PrimaryGeneratedColumn('uuid')
+  id: number
+
+  @Field()
+  @Column()
+  firstName: string
+
+  @Field()
+  @Column()
+  lastName: string
+
+  @Field()
+  fullName: string
+
+  @Field()
+  @Column('text', { unique: true })
+  email: string
+
+  @Field()
+  @Column()
+  password: string
+
+  permissions: [Permission]
+}
+
+export default User
